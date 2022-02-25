813d247fd877a0fe526228c67086bf3d1dbe2c65
RafaeLovera6 <rafaelovera6@gmail.com>
2022-02-25 21:11:08 +0000

    navbar ready missing paths and logo

---
+++ src/front/js/component/NavItems.js
@@ -0,0 +1,49 @@
+//Missing putting the all the paths
+
+export const navItems = [
+  {
+    id: 1,
+    title: "Home",
+    path: "./",
+    cName: "nav-item",
+  },
+  {
+    id: 2,
+    title: "Events",
+    path: "./",
+    cName: "nav-item",
+  },
+  {
+    id: 3,
+    title: "About [AppName]",
+    path: "./",
+    cName: "nav-item",
+  },
+  {
+    id: 4,
+    title: "Contact Us",
+    path: "./",
+    cName: "nav-item",
+  },
+];
+
+export const serviceDropdown = [
+  {
+    id: 1,
+    title: "Sign up for an event",
+    path: "./",
+    cName: "submenu-item"
+  },
+  {
+    id: 2,
+    title: "Event Details",
+    path: "./",
+    cName: "submenu-item",
+  },
+  {
+    id: 3,
+    title: "Admin Login",
+    path: "./",
+    cName: "submenu-item",
+  }
+];
