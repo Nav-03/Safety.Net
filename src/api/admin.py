59faab14711a2f058ce36162cb6dfd8cdce403bd
Nav-03 <navil.gabrie@gmail.com>
2022-02-25 01:44:04 +0000

    added framework for /routes.py

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, Coordinator
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -11,7 +11,14 @@ def setup_admin(app):
 
     
     # Add your models here, for example this is how we add a the User model to the admin
-    admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Coordinator, db.session))
+    admin.add_view(ModelView(Event_Coordinator, db.session))
+    admin.add_view(ModelView(Event, db.session))
+    admin.add_view(ModelView(Room, db.session))
+    admin.add_view(ModelView(Token, db.session))
+    # admin.add_view(ModelView(TokenPermission, db.session))
+    admin.add_view(ModelView(Permission, db.session))
+    
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,13 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, Coordinator
+from api.models import db, Event_Coordinator
+from api.models import db, Event
+from api.models import db, Room
+from api.models import db, Token
+from api.models import db, TokenPermission
+from api.models import db, Permission
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +22,75 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+
+@api.route('/Coordinator', methods=['POST'])
+def create_Coordinator():
+    Coordinator = Coordinator(email=email, password=password)
+    db.session.add(Coordinator)
+    db.session.commit()
+    return jsonify(Coordinator.serialize())
+
+
+@api.route('/Event_Coordinator', methods=['GET'])
+def Event_Coordinator():
+    Coordinator = Coordinator(email=email, password=password)
+    db.session.add(Event_Coordinator)
+    db.session.commit()
+    return jsonify(Event_Coordinator)
+
+
+@api.route('/Event', methods=['GET'])
+def Event():
+    Coordinator = Coordinator(email=email, password=password)
+    db.session.add(Event)
+    db.session.commit()
+    return jsonify(Event)
+
+   
+@api.route('/Room', methods=['GET'])
+def Room():
+    Coordinator = Coordinator(email=email, password=password)
+    db.session.add(Room)
+    db.session.commit()
+    return jsonify(Room)
+
+
+@api.route('/TokenPermission', methods=['POST'])
+def TokenPermission():
+    Coordinator = Coordinator(email=email, password=password)
+    db.session.add(TokenPermission)
+    db.session.commit()
+    return jsonify(TokenPermission)    
+
+@api.route('/Permission', methods=['POST'])
+def Permission():
+    Coordinator = Coordinator(email=email, password=password)
+    db.session.add(Permission)
+    db.session.commit()
+    return jsonify(Permission)
+
+
+
+@api.route('/Token', methods=['GET'])
+def create_Coordinator():
+    Coordinator = Coordinator(email=email, password=password)
+    db.session.add(Token)
+    db.session.commit()
+    return jsonify(Token)
+
+
+
+
+@api.route('/token', methods=['POST'])
+def create_token():
+    if request.json is None:
+        return jsonify({"msg":"Missing the payload"}), 400
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        return jsonify({"msg": "Missing email or password"}), 401
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
