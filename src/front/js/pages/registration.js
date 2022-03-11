ba3d52a0727ca03c4834e47f7aeb7a6b0d12a82f
Nav-03 <navil.gabrie@gmail.com>
2022-03-11 18:01:41 +0000

    edited dropdown on registration form

--- src/front/js/pages/registration.js
+++ src/front/js/pages/registration.js
@@ -49,28 +49,28 @@ export const Registration = (props) => {
                 placeholder="Enter your email"
                 required
               />
-              {/* </div>
-        <div className="input-box">
-            <input type="password" placeholder="Create password" required/>
-        </div>
-        <div className="input-box">
-            <input type="password" placeholder="Confirm password" required/> */}
             </div>
-            <div className="policy">
-              <input type="checkbox" />
-              <h3>VIP</h3>
-              <input type="checkbox" />
-              <h3>Valet</h3>
-              <input type="checkbox" />
-              <h3>Unlimited Drinks</h3>
-              <input type="checkbox" />
-              <h3>Dinner</h3>
+
+            <div className="text">
+              {/* <h3>Already have an account? <a href="#">Login now</a></h3> */}
+            </div>
+
+            <div class="dropdown">
+              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown button
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><div className="dropdown-item" ><input type="checkbox" />
+                  <h3>VIP</h3></div></li>
+                <li><div className="dropdown-item" ><input type="checkbox" />
+                  <h3>Valet</h3></div></li>
+                <li><div className="dropdown-item" ><input type="checkbox" />
+                  <h3>Dinner</h3></div></li>
+              </ul>
+
             </div>
             <div className="input-box button">
               <input type="Submit" value="Register Now" />
-            </div>
-            <div className="text">
-              {/* <h3>Already have an account? <a href="#">Login now</a></h3> */}
             </div>
           </form>
         </div>
