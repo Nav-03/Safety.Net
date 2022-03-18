84284ad0b801a368c15ce2e747df954c767b9935
RafaeLovera6 <rafaelovera6@gmail.com>
2022-03-18 02:24:34 +0000

    Changed the footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a className="text-light" href="http://www.4geeksacademy.com">4Geeks Students</a>
-		</p>
-	</footer>
+  <footer className="footer mt-auto py-3 text-center">
+    <p>
+      Made with <i className="fa fa-heart text-danger" /> by{" "}
+      <a className="text-light" href="http://www.4geeksacademy.com">
+        3Geeks Students
+      </a>
+    </p>
+  </footer>
 );
